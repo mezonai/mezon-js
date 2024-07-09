485ae80900df3bbfcc8d962adf41588b1fcf53ec
tranhuythang9999rgb <thang.tranhuy@ncc.asia>
2024-07-09 16:08:32 +0700

    add crud emoji

--- packages/mezon-js/client.ts
+++ packages/mezon-js/client.ts
@@ -96,7 +96,8 @@ import {
   ApiChangeChannelPrivateRequest,
   ApiClanEmojiList,
   ApiClanEmojiCreateRequest,
-  ApiChannelVoiceList
+  ApiChannelVoiceList,
+  MezonUpdateClanEmojiByIdBody
 } from "./api.gen";
 
 import { Session } from "./session";
@@ -2257,5 +2258,23 @@ async createClanEmoji(session: Session, request: ApiClanEmojiCreateRequest) {
     return response !== undefined;
   });
 }
-
+//**update clan emoji */
+async updateClanEmojiById(session :Session,id:string,request: MezonUpdateClanEmojiByIdBody){
+  if (this.autoRefreshSession && session.refresh_token &&
+    session.isexpired((Date.now() + this.expiredTimespanMs)/1000)) {
+    await this.sessionRefresh(session);
+  }
+  return this.apiClient.updateClanEmojiById(session.token,id,request).then((response: any) => {
+    return response !== undefined;
+  });
+}
+async deleteByIdClanEmoji(session:Session,id:string){
+  if (this.autoRefreshSession && session.refresh_token &&
+    session.isexpired((Date.now() + this.expiredTimespanMs)/1000)) {
+    await this.sessionRefresh(session);
+  }
+  return this.apiClient.deleteByIdClanEmoji(session.token,id).then((response: any) => {
+    return response !== undefined;
+  });
+}
 };
