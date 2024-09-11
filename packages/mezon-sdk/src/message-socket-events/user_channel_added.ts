0ede7bbbcc8513bd94a656b8070f66e8db016d7d
tuan vu <son.nguyenhoai@ncc.asia>
2024-09-11 17:44:04 +0700

    update bot send message to user

--- packages/mezon-sdk/src/message-socket-events/user_channel_added.ts
+++ packages/mezon-sdk/src/message-socket-events/user_channel_added.ts
@@ -1,10 +1,21 @@
+import {
+  InternalEventsSocket,
+} from "../constants";
 import { BaseSocketEvent } from "./base_event";
 
-export class UserChannelAdded extends BaseSocketEvent{
-    public handleFunctions: ((...args: any[]) => void)[] = [this.connectChatClan];
-    public event: string = '';
-    
-    connectChatClan(input : any){
-        console.log(input);
-    }
+export class UserChannelAdded extends BaseSocketEvent {
+  public handleFunctions: ((...args: any[]) => void)[] = [this.joinChannel];
+  public event: string = InternalEventsSocket.UserChannelAddedEvent;
+
+  async joinChannel(input: any) {
+    console.log(input);
+    // await this.socket.joinChat(
+    //   input.clan_id,
+    //   input.parent_id,
+    //   input.channel_id,
+    //   input.channel_type,
+    //   input.is_public,
+    //   input.is_parent_public
+    // );
+  }
 }
--- packages/mezon-sdk/src/utils/helper.ts
+++ packages/mezon-sdk/src/utils/helper.ts
@@ -1,5 +1,19 @@
-import { InternalEventsSocket } from "../constants";
+import { ChannelStreamMode, ChannelType, InternalEventsSocket } from "../constants";
 
 export function convertInternalEventToEvents(input :InternalEventsSocket) : string{
     return input.replace(/_event/g, '').replace(/_/g, '');
+}
+
+
+export function convertChanneltypeToChannelMode(channelType : string | number){
+    switch (Number(channelType)) {
+      case ChannelType.CHANNEL_TYPE_DM:
+        return ChannelStreamMode.STREAM_MODE_DM;
+      case ChannelType.CHANNEL_TYPE_GROUP:
+        return ChannelStreamMode.STREAM_MODE_GROUP;
+      case ChannelType.CHANNEL_TYPE_TEXT:
+        return ChannelStreamMode.STREAM_MODE_CHANNEL;
+    }
+
+    return false;
 }
