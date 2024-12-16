87346f1b13dc0c9ff235dd07b28d00e9c56dcdb1
michael.mai <michael.mai.thatera@gmail.com>
2024-12-16 11:21:34 +0700

    remove check socket isOpen on ondisconenct

--- packages/mezon-sdk/src/client.ts
+++ packages/mezon-sdk/src/client.ts
@@ -269,9 +269,7 @@ export class MezonClient implements Client {
   onerror(evt: ErrorEvent) {
     console.log(evt);
     if (this.isHardDisconnect) return;
-    if (this.socket.isOpen()) {
-      this.retriesConnect();
-    }
+    this.retriesConnect();
   }
 
   onheartbeattimeout() {
@@ -308,9 +306,7 @@ export class MezonClient implements Client {
             // Clear the current interval and restart with the new retry interval
             clearInterval(interval);
             setTimeout(() => {
-              if (this.socket.isOpen()) {
-                this.retriesConnect(); // Restart the connection attempt
-              }
+              this.retriesConnect(); // Restart the connection attempt
             }, retryInterval);
         }
     }, retryInterval);
