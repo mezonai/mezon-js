80ffa51e83e62d19d181b3f95e28bb83383ad370
Nhan Nguyen <nguyentran@ncc.asia>
2025-10-13 10:17:13 +0700

    add upload oauth file

--- packages/mezon-js/api.gen.ts
+++ packages/mezon-js/api.gen.ts
@@ -9502,6 +9502,42 @@ export class MezonApi {
   }
 
   /** Upload attachment */
+  uploadOauthFile(bearerToken: string,
+      body:ApiUploadAttachmentRequest,
+      options: any = {}): Promise<ApiUploadAttachment> {
+    
+    if (body === null || body === undefined) {
+      throw new Error("'body' is a required parameter but is null or undefined.");
+    }
+    const urlPath = "/v2/uploadoauthfile";
+    const queryParams = new Map<string, any>();
+
+    let bodyJson : string = "";
+    bodyJson = JSON.stringify(body || {});
+
+    const fullUrl = this.buildFullUrl(this.basePath, urlPath, queryParams);
+    const fetchOptions = buildFetchOptions("POST", options, bodyJson);
+    if (bearerToken) {
+        fetchOptions.headers["Authorization"] = "Bearer " + bearerToken;
+    }
+
+    return Promise.race([
+      fetch(fullUrl, fetchOptions).then((response) => {
+        if (response.status == 204) {
+          return response;
+        } else if (response.status >= 200 && response.status < 300) {
+          return response.json();
+        } else {
+          throw response;
+        }
+      }),
+      new Promise((_, reject) =>
+        setTimeout(reject, this.timeoutMs, "Request timed out.")
+      ),
+    ]);
+  }
+
+  /** Upload attachment */
   uploadAttachmentFile(
     bearerToken: string,
     body: ApiUploadAttachmentRequest,
