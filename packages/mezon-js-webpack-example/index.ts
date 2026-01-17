4f4fcceb9a989876d04cecafde687476777c5e95
Nhan Nguyen <nguyentran@ncc.asia>
2026-01-17 16:01:08 +0700

    add test func

--- packages/mezon-js-webpack-example/index.ts
+++ packages/mezon-js-webpack-example/index.ts
@@ -21,7 +21,7 @@ var useSSL = true; // Enable if server is run with an SSL certificate.
 var clientgw = new Client("defaultkey", "dev-mezon.nccsoft.vn", "8088", useSSL);
 var client = new Client("defaultkey", "172.16.11.90", "7350", false);
 
-clientgw.authenticateEmail("pocolomos@gmail.com", "xxxxxxxxxx").then(async session => {
+clientgw.authenticateEmail("pocolomos@gmail.com", "xxxxxxxxx").then(async session => {
   console.log("authenticated.", session);
 
   console.log(await client.listChannelMessages(session, 1775732550744936448n, 1803320023704408064n))
@@ -33,9 +33,9 @@ clientgw.authenticateEmail("pocolomos@gmail.com", "xxxxxxxxxx").then(async sessi
   const socket = client.createSocket(false, true, new WebSocketAdapterPb());
   const session2 = await socket.connect(session, true, "desktop");
   
-  console.log('join clan', await socket.joinClanChat(0n));
+  //console.log('join clan', await socket.joinClanChat(0n));
   console.log('join clan', await socket.joinClanChat(1775732550744936448n));
-  console.log("session", session2);
+  //console.log("session", session2);
   const resp = await socket.listDataSocket(
     { 
       api_name: "ListClanDescs",
@@ -46,6 +46,8 @@ clientgw.authenticateEmail("pocolomos@gmail.com", "xxxxxxxxxx").then(async sessi
     }
   );
   console.log('resp', resp);
+
+  await socket.writeChatMessage(1775732550744936448n, 1803320023704408064n, 2, false, {'t':'hello'});
 
 }).catch(e => {
   console.log("error authenticating.");
