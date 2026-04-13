f9085f8773be3416e836f0b74938e4b8b87176e0
Nhan Nguyen <nguyentran@ncc.asia>
2026-04-13 20:00:09 +0700

    fix compile

--- packages/js-native/index.js
+++ packages/js-native/index.js
@@ -1,2 +1,13 @@
-const native = require('./build/Release/mezon_native.node');
+let native = null;
+
+if (typeof process !== 'undefined' && process.versions && process.versions.node) {
+    try {
+        native = require('./build/Release/mezon_native.node');
+    } catch (err) {
+        console.error("Native module loading failed", err);
+    }
+} else {
+    console.warn("js-native is not supported in this environment (Browser).");
+}
+
 module.exports = native;
