f20e706a82fd3776e61bd2884854ce711d875a62
Nhan Nguyen <nguyentran@ncc.asia>
2026-04-13 18:15:28 +0700

    add abridged_tcp_adapter

--- packages/mezon-js-protobuf/web_socket_adapter_pb.ts
+++ packages/mezon-js-protobuf/web_socket_adapter_pb.ts
@@ -14,78 +14,12 @@
  * limitations under the License.
  */
 import * as tsproto from "./rtapi/realtime";
-
-/**
- * An interface used by Mezon's web socket to determine the payload protocol.
- */
-export interface WebSocketAdapter {
-  /**
-   * Dispatched when the web socket closes.
-   */
-  onClose: SocketCloseHandler | null;
-
-  /**
-   * Dispatched when the web socket receives an error.
-   */
-  onError: SocketErrorHandler | null;
-
-  /**
-   * Dispatched when the web socket receives a normal message.
-   */
-  onMessage: SocketMessageHandler | null;
-
-  /**
-   * Dispatched when the web socket opens.
-   */
-  onOpen: SocketOpenHandler | null;
-
-  isOpen(): boolean;
-  close(): void;
-  connect(
-    scheme: string,
-    host: string,
-    port: string,
-    createStatus: boolean,
-    token: string,
-    platform: string,
-    signal?: AbortSignal,
-  ): void;
-  send(message: any): void;
-}
-
-/**
- * SocketCloseHandler defines a lambda that handles WebSocket close events.
- */
-export interface SocketCloseHandler {
-  (this: WebSocket, evt: CloseEvent): void;
-}
-
-/**
- * SocketErrorHandler defines a lambda that handles responses from the server via WebSocket
- * that indicate an error.
- */
-export interface SocketErrorHandler {
-  (this: WebSocket, evt: Event): void;
-}
-
-/**
- * SocketMessageHandler defines a lambda that handles valid WebSocket messages.
- */
-export interface SocketMessageHandler {
-  (message: any): void;
-}
-
-/**
- * SocketOpenHandler defines a lambda that handles WebSocket open events.
- */
-export interface SocketOpenHandler {
-  (this: WebSocket, evt: Event): void;
-}
+import { SocketCloseHandler, SocketErrorHandler, SocketMessageHandler, SocketOpenHandler, TransportAdapter } from "./transport_adapter";
 
 /**
  * A protocol buffer socket adapter that accepts and transmits payloads using the protobuf binary wire format.
  */
-export class WebSocketAdapterPb implements WebSocketAdapter {
+export class WebSocketAdapterPb implements TransportAdapter {
   private _socket?: WebSocket;
 
   constructor() { }
