bf0e65f543e102afa1e0cb7f44af7db76af04f9c
Nhan Nguyen <nguyentran@ncc.asia>
2026-04-14 08:50:34 +0700

    fix include native code

--- packages/js-native/index.js
+++ packages/js-native/index.js
@@ -1,8 +1,11 @@
 let native = null;
 
-if (typeof process !== 'undefined' && process.versions && process.versions.node) {
+const isNode = typeof process !== 'undefined' && process.versions && !!process.versions.node;
+
+if (isNode) {
     try {
-        native = require('./build/Release/mezon_native.node');
+        const BINARY_PATH = './build/Release/mezon_native.node';
+        native = require(BINARY_PATH);
     } catch (err) {
         console.error("Native module loading failed", err);
     }
